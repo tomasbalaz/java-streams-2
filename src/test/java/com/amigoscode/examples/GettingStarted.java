cdf4299acef9a29858afa0cafc5fffa6438edd53
Tomas Balaz <balaztomas0@gmail.com>
2023-09-01 15:32:24 +0200

    Imperative and Declarative approach of same problem in class GettingStarted

--- src/test/java/com/amigoscode/examples/GettingStarted.java
+++ src/test/java/com/amigoscode/examples/GettingStarted.java
@@ -17,10 +17,46 @@ public class GettingStarted {
         // 1. Find people aged less or equal 18
         // 2. Then change implementation to find first 10 people
         List<Person> people = MockData.getPeople();
+        List<Person> peopleLessOrEqualThan18 = new ArrayList<>();
+        List<Person> peopleLessOrEqualThan18_2 = new ArrayList<>();
+
+        //1st solution
+        for(Person person : people) {
+            if(person.getAge() <= 18) {
+                peopleLessOrEqualThan18.add(person);
+            }
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(peopleLessOrEqualThan18.get(i));
+        }
+
+        System.out.println();
+
+        //2nd solution
+        int limit = 10;
+        int counter = 0;
+        for(Person person : people) {
+            if(person.getAge() <= 18) {
+                peopleLessOrEqualThan18_2.add(person);
+                counter++;
+                if (counter == limit) {
+                    break;
+                }
+            }
+        }
+        peopleLessOrEqualThan18_2.forEach(System.out::println);
+
     }
 
     @Test
     public void declarativeApproachUsingStreams() throws Exception {
         List<Person> people = MockData.getPeople();
+        List<Person> youngPeople = people.stream()
+                .filter(person -> person.getAge() <= 18)
+                .limit(10)
+                .toList();
+
+        youngPeople.forEach(System.out::println);
     }
 }
