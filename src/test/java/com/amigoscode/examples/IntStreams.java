a38b87a497f52ca7ab91dc2c11d73127c468ec63
Tomas Balaz <balaztomas0@gmail.com>
2023-09-04 15:02:38 +0200

    Used IntStream.range method for iterating over another List of Objects

--- src/test/java/com/amigoscode/examples/IntStreams.java
+++ src/test/java/com/amigoscode/examples/IntStreams.java
@@ -5,6 +5,7 @@ import com.amigoscode.beans.Person;
 import com.amigoscode.mockdata.MockData;
 import org.junit.jupiter.api.Test;
 
+import java.lang.module.FindException;
 import java.util.List;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -31,6 +32,10 @@ public class IntStreams {
     @Test
     public void rangeIteratingLists() throws Exception {
         List<Person> people = MockData.getPeople();
+        IntStream.range(0, people.size())
+                .forEach(index -> {
+                    System.out.println(people.get(index));
+                });
     }
 
     @Test
