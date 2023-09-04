1958485ae295c8b252789bbcaaa7e149806e839f
Tomas Balaz <balaztomas0@gmail.com>
2023-09-04 15:24:20 +0200

    Example of IntStream.min, IntStream.max,

--- src/test/java/com/amigoscode/examples/MinMax.java
+++ src/test/java/com/amigoscode/examples/MinMax.java
@@ -10,10 +10,22 @@ public class MinMax {
     @Test
     public void min() {
         List<Integer> numbers = List.of(1, 2, 3, 100, 23, 93, 99);
+
+        Integer min = numbers.stream()
+                //.min((o1, o2) -> o1.compareTo(o2))
+                //.min(Integer::compareTo)
+                .min(Comparator.naturalOrder())
+                .get();
+        System.out.println(min);
     }
 
     @Test
     public void max() {
         List<Integer> numbers = List.of(1, 2, 3, 100, 23, 93, 99);
+
+        Integer max = numbers.stream()
+                .max(Comparator.naturalOrder())
+                .get();
+        System.out.println(max);
     }
 }
