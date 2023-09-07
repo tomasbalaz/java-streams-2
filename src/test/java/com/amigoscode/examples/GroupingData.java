e58d7b14d2a7b0d66140952dd41612dcf93f7f58
Tomas Balaz <balaztomas0@gmail.com>
2023-09-07 15:23:46 +0200

    Example of how to use Collectors.groupingBy and Collectors.groupingBy with counting

--- src/test/java/com/amigoscode/examples/GroupingData.java
+++ src/test/java/com/amigoscode/examples/GroupingData.java
@@ -15,6 +15,14 @@ public class GroupingData {
     @Test
     public void simpleGrouping() throws Exception {
         List<Car> cars = MockData.getCars();
+        Map<String, List<Car>> map = cars.stream()
+                .collect(Collectors.groupingBy(Car::getMake));
+
+        map.forEach((s, cars1) -> {
+            System.out.println("Make " + s);
+            cars1.forEach(System.out::println);
+            System.out.println("---------------------");
+        });
     }
 
     @Test
@@ -30,6 +38,11 @@ public class GroupingData {
                 "Alex",
                 "Alex"
         );
+        Map<String, Long> map = names.stream()
+                //.collect(Collectors.groupingBy(s -> s, Collectors.counting()));
+                        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        System.out.println(map);
     }
 
 }
