1cdfce7a2e64577c61df74f72fadbedc50c33f0b
Tomas Balaz <balaztomas0@gmail.com>
2023-09-07 14:58:41 +0200

    Example of how to use min, max, average, sum, DoubleSummaryStatistics

--- src/test/java/com/amigoscode/examples/StatisticsWithStreams.java
+++ src/test/java/com/amigoscode/examples/StatisticsWithStreams.java
@@ -8,38 +8,76 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 import java.util.DoubleSummaryStatistics;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class StatisticsWithStreams {
 
     @Test
     public void count() throws Exception {
         List<Car> cars = MockData.getCars();
+        long count = cars.stream()
+                .filter(car -> car.getMake().equalsIgnoreCase("ford"))
+                .filter(car -> car.getYear() > 2010)
+                .count();
+        System.out.println(count);
+
+        AtomicInteger counter = new AtomicInteger();
+        cars.forEach(car -> {
+            if(car.getMake().equalsIgnoreCase("ford")) {
+                counter.getAndIncrement();
+            }
+        });
+        System.out.println(counter);
+
     }
 
     @Test
     public void min() throws Exception {
         List<Car> cars = MockData.getCars();
+        double min = cars.stream()
+                .mapToDouble(Car::getPrice)
+                .min()
+                .orElse(0);
+        System.out.println(min);
     }
 
     @Test
     public void max() throws Exception {
         List<Car> cars = MockData.getCars();
+        double max = cars.stream()
+                .mapToDouble(Car::getPrice)
+                .max()
+                .orElse(0);
+        System.out.println(max);
     }
 
 
     @Test
     public void average() throws Exception {
         List<Car> cars = MockData.getCars();
+        double average = cars.stream()
+                .mapToDouble(Car::getPrice)
+                .average()
+                .orElse(0);
+        System.out.println(average);
     }
 
     @Test
     public void sum() throws Exception {
         List<Car> cars = MockData.getCars();
+        double sum = cars.stream()
+                .mapToDouble(Car::getPrice)
+                .sum();
+        System.out.println(BigDecimal.valueOf(sum));
     }
 
     @Test
     public void statistics() throws Exception {
         List<Car> cars = MockData.getCars();
+        DoubleSummaryStatistics statistics = cars.stream()
+                .mapToDouble(Car::getPrice)
+                .summaryStatistics();
+        System.out.println(statistics);
     }
 
 }
