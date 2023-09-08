c99cc0775614e6e7a7dde476d9d762c67e23fdab
Tomas Balaz <balaztomas0@gmail.com>
2023-09-08 13:37:36 +0200

    Example of how to sort by property, by object

--- src/test/java/com/amigoscode/examples/Sorting.java
+++ src/test/java/com/amigoscode/examples/Sorting.java
@@ -15,21 +15,51 @@ public class Sorting {
     @Test
     public void sortingSteamOfElements() throws IOException {
         List<Person> people = MockData.getPeople();
+        List<String> sorted = people.stream()
+                .map(Person::getFirstName)
+                .sorted()
+                .toList();
+        System.out.println(sorted);
     }
 
     @Test
     public void sortingSteamOfElementsReverse() throws IOException {
         List<Person> people = MockData.getPeople();
+        List<String> reversed = people.stream()
+                .map(Person::getFirstName)
+                .sorted(Comparator.reverseOrder())
+                .toList();
+        System.out.println(reversed);
     }
 
     @Test
     public void sortingSteamOfObjets() throws IOException {
         List<Person> people = MockData.getPeople();
+
+        Comparator<Person> comparing = Comparator
+                .comparing(Person::getEmail)
+                .reversed()
+                .thenComparing(Person::getFirstName);
+        //Comparator<Person> comparing = Comparator.comparing(Person::getFirstName).reversed();
+
+        List<Person> sorted = people.stream()
+                .sorted(comparing)
+                .toList();
+
+        sorted.forEach(System.out::println);
     }
 
     @Test
     public void topTenMostExpensiveBlueCars() throws IOException {
         List<Car> cars = MockData.getCars();
+        List<Car> topTenMostExpensiveBlueCars = cars.stream()
+                .filter(car -> car.getColor().equalsIgnoreCase("blue"))
+                .sorted(Comparator.comparing(Car::getPrice).reversed())
+                .limit(10)
+                .toList();
+
+        topTenMostExpensiveBlueCars.forEach(System.out::println);
+
     }
 
 }
