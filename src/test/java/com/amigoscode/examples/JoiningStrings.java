066b092382338a2f93edaf4314f04d0f44190eb4
Tomas Balaz <balaztomas0@gmail.com>
2023-09-08 14:05:26 +0200

    Example of how join String without and with streams

--- src/test/java/com/amigoscode/examples/JoiningStrings.java
+++ src/test/java/com/amigoscode/examples/JoiningStrings.java
@@ -10,12 +10,28 @@ public class JoiningStrings {
     @Test
     public void joiningStrings() throws Exception {
         List<String> names = List.of("anna", "john", "marcos", "helena", "yasmin");
+
+        StringBuilder join = new StringBuilder();
+
+        for(String name: names) {
+            join.append(name.substring(0,1).toUpperCase())
+                    .append(name.substring(1))
+                    .append(", ");
+        }
+        System.out.println(join);
         // "Anna, John, Marcos, Helena, Yasmin"
     }
 
     @Test
     public void joiningStringsWithStream() throws Exception {
         List<String> names = List.of("anna", "john", "marcos", "helena", "yasmin");
+
+        String upperCaseNames = names.stream()
+                .map(name -> name.substring(0, 1).toUpperCase() + name.substring(1))
+                .collect(Collectors.joining(", "));
+
+        System.out.println(upperCaseNames);
+
         // "Anna, John, Marcos, Helena, Yasmin"
 
     }
